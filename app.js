60d3156f3f5350e7487b6a8a3025c8a8f4952a72
cfddream <cfddream@gmail.com>
2012-07-28 16:24:50 +0800

    add `about` page

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
+app.get('/about', routes.about);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,12 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Beauty Of The Love ♥ 美丽的爱情' });
 };
+
+
+/*
+ * GET about page.
+ */
+
+exports.about = function (req, res) {
+  res.render('about', { title: 'Beauty Of The Love ♥ 美丽的爱情' });
+};
